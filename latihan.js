10a7009e72e8bfcee904f1931a0fcd987d271001
iamtaufik <muhtopik07@gmail.com>
2023-09-08 20:58:48 +0700

    feat: menambahkan fungsi hitung volume kerucut

--- latihan.js
+++ latihan.js
@@ -12,6 +12,10 @@ function hitungVolumeKubus(sisi) {
 
 var panjangSisi = 5;
 var volumeKubus = hitungVolumeKubus(panjangSisi);
-console.log(
-  "panjang sisi volume kubus " + panjangSisi + " adalah " + volumeKubus
-);
+console.log('panjang sisi volume kubus ' + panjangSisi + ' adalah ' + volumeKubus);
+
+// Muhammad Taufik Hidayat -> menghitung volume kerucut
+function hitungVolumeKerucut(r, t) {
+  return (1 / 3) * 3.14 * r * r * t;
+}
+console.log(hitungVolumeKerucut(7, 10));
