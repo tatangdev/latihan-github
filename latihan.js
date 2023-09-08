610eaa659f5719dd563622fd7ede8bf2d267eb4a
imran <muhammadalimran22@gmail.com>
2023-09-08 20:39:35 +0700

    feat: create function volume kubus

--- latihan.js
+++ latihan.js
@@ -1,6 +1,17 @@
 // Nama -> nama program
 // Tatang -> penjumlahan
 function penjumlahan(a, b) {
-    return a + b
+  return a + b;
 }
-console.log(penjumlahan(1, 2))
+console.log(penjumlahan(1, 2));
+
+//Muhammad Al Imran -> menghitung volume kubus
+function hitungVolumeKubus(sisi) {
+  return sisi * sisi * sisi;
+}
+
+var panjangSisi = 5;
+var volumeKubus = hitungVolumeKubus(panjangSisi);
+console.log(
+  "panjang sisi volume kubus " + panjangSisi + " adalah " + volumeKubus
+);
