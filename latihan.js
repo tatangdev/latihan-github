a29a3bfb06001131578b456873603304ceb0fd60
rian <rian.rafli@gmail.com>
2023-09-08 21:16:06 +0700

    feat : create function volume limas segitiga

--- latihan.js
+++ latihan.js
@@ -4,3 +4,14 @@ function penjumlahan(a, b) {
     return a + b
 }
 console.log(penjumlahan(1, 2))
+
+
+// Rian -> menghitung volume limas segitiga
+
+function volumeLimasSegitiga(tinggisegitiga,alasSegitiga,tinggiLimas) {
+    const luasAlas = 1/2 * alasSegitiga * tinggisegitiga
+    return 1/3 * luasAlas * tinggiLimas
+}
+
+
+console.log(volumeLimasSegitiga(10,10,12));
