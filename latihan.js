88af13434af72f5a712ce871f2941f7288ed6ee9
ismiazzah <ismiazzahk4@gmail.com>
2023-09-08 22:05:59 +0700

    resolve conflict

--- latihan.js
+++ latihan.js
@@ -15,3 +15,16 @@ var volumeKubus = hitungVolumeKubus(panjangSisi);
 console.log(
   "panjang sisi volume kubus " + panjangSisi + " adalah " + volumeKubus
 );
+
+//Ismiazzah Kamilah -> menghitung volume balok
+function hitungVolumeBalok(panjang, lebar, tinggi){
+  let volume = panjang * lebar * tinggi
+  return volume;
+}
+
+let panjangBalok = 5
+let lebarBalok = 3
+let tinggiBalok = 2
+
+let volumeBalok = hitungVolumeBalok(panjangBalok, lebarBalok, tinggiBalok)
+console.log("Volume balok adalah" + volumeBalok);
