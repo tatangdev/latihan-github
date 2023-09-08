cf87361742a79185cdcc5073ecddc2e7a2d5ce04
fathpurn <talitakamilia345@gmail.com>
2023-09-08 20:58:43 +0700

    feat: create function volume limas persegi

--- latihan.js
+++ latihan.js
@@ -4,3 +4,11 @@ function penjumlahan(a, b) {
     return a + b
 }
 console.log(penjumlahan(1, 2))
+
+
+// Purno -> Volume Limas Persegi
+function volumeLimasPersegi(s, t) {
+    return 1/3*(s**2)*t;
+} 
+
+console.log(`Volume Limas Persegi:  ${volumeLimasPersegi(5,6)}`);
