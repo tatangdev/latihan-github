2bc588aa8ff81eac9d30bcfc58e42ae73ce1b01c
Shera Alice <sheraalice@students.amikom.ac.id>
2023-09-08 22:03:37 +0700

    feat:Menghitung_Volume_Bola

--- latihan.js
+++ latihan.js
@@ -15,3 +15,13 @@ var volumeKubus = hitungVolumeKubus(panjangSisi);
 console.log(
   "panjang sisi volume kubus " + panjangSisi + " adalah " + volumeKubus
 );
+
+//Shera Alice Ayutri -> menghitung volume bola
+function volumeBola(jariJari) {
+    const pi = 3.14;
+    const volume = (4 / 3) * pi * jariJari ** 3;
+    return volume;
+  }
+const jariJariBola = 10;
+const hasilVolume = volumeBola(jariJariBola);
+console.log(`Volume bola dengan jari-jari ${jariJariBola} adalah ${hasilVolume}`);
