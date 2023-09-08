13930623c89b56190e622b6fa254b9ad669a3e0d
Arfin Dwi Octavianto <arfindwioctavianto@gmail.com>
2023-09-08 20:46:23 +0700

    feat: buat fungsi luas segitiga

--- latihan.js
+++ latihan.js
@@ -1,6 +1,12 @@
 // Nama -> nama program
 // Tatang -> penjumlahan
 function penjumlahan(a, b) {
-    return a + b
+  return a + b;
 }
-console.log(penjumlahan(1, 2))
+console.log(penjumlahan(1, 2));
+
+// Arfin -> Luas Segitiga
+function luasSegitiga(a, b) {
+  return 0.5 * a * b;
+}
+console.log(luasSegitiga(5, 10));
