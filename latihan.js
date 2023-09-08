848bbb6d37ce289c0a19f17cd7d426f3a66634c2
Hafid <hafid@gmail.com>
2023-09-08 21:14:53 +0700

    s

--- latihan.js
+++ latihan.js
@@ -1,6 +1,14 @@
 // Nama -> nama program
 // Tatang -> penjumlahan
 function penjumlahan(a, b) {
-    return a + b
+  return a + b;
 }
-console.log(penjumlahan(1, 2))
+console.log(penjumlahan(1, 2));
+
+// Hafid Nur-Keliling jajar genhang
+//
+
+function KjajarGenjang(sisi_satu, sisi_dua) {
+  return 2 * (sisi_satu + sisi_dua);
+}
+console.log(KjajarGenjang(10,10))
