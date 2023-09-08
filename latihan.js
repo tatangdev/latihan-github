910d64f771dcc1801ae8c7e79564c766803ca8db
nuruldwia <nuruldwia@students.unnes.ac.id>
2023-09-08 21:24:35 +0700

    feat: create volume prism

--- latihan.js
+++ latihan.js
@@ -4,3 +4,10 @@ function penjumlahan(a, b) {
     return a + b
 }
 console.log(penjumlahan(1, 2))
+
+
+// Nurul -> Fungsi Volume Prisma
+function volumePrisma(luasAlas, tinggi) {
+    return luasAlas * tinggi;
+}
+console.log(volumePrisma(20, 5));
